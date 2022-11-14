7411e6f98017c80af8520e56d42b6533dc3c441c
Yur4ik123 <yur.malishko@gmail.com>
2022-11-14 12:19:20 +0200

    add

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import UIkit from 'uikit';
+import axios from "axios";
 import './charts'
 let coll = document.getElementsByClassName("collapsible");
 let i;
@@ -14,4 +15,35 @@ for (i = 0; i < coll.length; i++) {
             content.style.maxHeight = content.scrollHeight + "px";
         }
     });
+}
+if (document.getElementById('sec-items-wrapper')) {
+    axios.get('https://data.sec.gov/submissions/CIK0001697412.json').then(res => {
+        console.log(res.data.filings.recent);
+        let recent = res.data.filings.recent || [];
+        let arr = [];
+        console.log(recent);
+        recent.isXBRL.forEach(function (elem, idx) {
+            if (elem == 1) {
+                arr.push({
+                    number: recent.accessionNumber[idx],
+                    date: recent.filingDate[idx],
+                    form:recent.form[idx],
+                    description: recent.primaryDocDescription[idx],
+                    doc:recent.primaryDocument[idx],
+                    url:'https://www.sec.gov/ix?doc=/Archives/edgar/data/'+res.data.cik+'/'+recent.accessionNumber[idx].replaceAll('-', '')+'/'+recent.primaryDocument[idx],
+                })
+            }
+        });
+        let html = '';
+        arr.forEach(function(elem, idx){
+            html+='            <div class="table__row">\n' +
+                '                <div class="col">'+elem.date+'</div>\n' +
+                '                <div class="col">'+elem.form+'</div>\n' +
+                '                <div class="col">'+elem.description+'</div>\n' +
+                '                <div class="col">                    <a href="'+elem.url+'" target="_blank"><img src="/assets/img/link.svg" width="20" height="20" alt=""></a>\n' +
+                '                </div>\n' +
+                '            </div>';
+        });
+        document.querySelector('#sec-table-list').innerHTML = html;
+    })
 }
--- resources/views/sec.blade.php
+++ resources/views/sec.blade.php
@@ -3,69 +3,21 @@
 @section('content')
     <div class="page__heading">
         <div class="container">
-        <h1 class="page__header">SEC Filings</h1>
+            <h1 class="page__header">SEC Filings</h1>
         </div>
     </div>
     <div class="container">
-    <div class="sec__items-wrapper">
-        <div class="sec__table-heading table__row">
-            <div class="col">Date</div>
-            <div class="col">Filing Type</div>
-            <div class="col">Filing Description</div>
-            <div class="col">Download / View</div>
-        </div>
-        <div class="sect__table-list ">
-            <div class="table__row">
-                <div class="col">11/03/2022</div>
-                <div class="col">4</div>
-                <div class="col">Statement of Changes in Beneficial Ownership</div>
-                <div class="col">                    <a href="" target="_blank"><img src="/assets/img/link.svg" width="20" height="20" alt=""></a>
-                </div>
+        <div class="sec__items-wrapper" id="sec-items-wrapper">
+            <div class="sec__table-heading table__row">
+                <div class="col">Date</div>
+                <div class="col">Filing Type</div>
+                <div class="col">Filing Description</div>
+                <div class="col">Download / View</div>
             </div>
-            <div class="table__row">
-                <div class="col">11/03/2022</div>
-                <div class="col">4</div>
-                <div class="col">Statement of Changes in Beneficial Ownership</div>
-                <div class="col">
-                    <a href="" target="_blank"><img src="/assets/img/link.svg" width="20" height="20" alt=""></a>
+            <div class="sect__table-list " id="sec-table-list">
 
-                </div>
             </div>
-            <div class="table__row">
-                <div class="col">11/03/2022</div>
-                <div class="col">4</div>
-                <div class="col">Statement of Changes in Beneficial Ownership</div>
-                <div class="col">                    <a href="" target="_blank"><img src="/assets/img/link.svg" width="20" height="20" alt=""></a>
-                </div>
-            </div>
-            <div class="table__row">
-                <div class="col">11/03/2022</div>
-                <div class="col">4</div>
-                <div class="col">Statement of Changes in Beneficial Ownership</div>
-                <div class="col">
-                    <a href="" target="_blank"><img src="/assets/img/link.svg" width="20" height="20" alt=""></a>
-
-                </div>
-            </div>    <div class="table__row">
-                <div class="col">11/03/2022</div>
-                <div class="col">4</div>
-                <div class="col">Statement of Changes in Beneficial Ownership</div>
-                <div class="col">                    <a href="" target="_blank"><img src="/assets/img/link.svg" width="20" height="20" alt=""></a>
-                </div>
-            </div>
-            <div class="table__row">
-                <div class="col">11/03/2022</div>
-                <div class="col">4</div>
-                <div class="col">Statement of Changes in Beneficial Ownership</div>
-                <div class="col">
-                    <a href="" target="_blank"><img src="/assets/img/link.svg" width="20" height="20" alt=""></a>
-
-                </div>
-            </div>
-
 
         </div>
-
-    </div>
     </div>
 @endsection
